61f2b138555a2f323c8a55e077623b82ade52909
Serj Kostyshyn <sergkostyshyn@gmail.com>
2021-05-16 19:23:21 +0300

    test

--- App/SoundEffects/GameMove/index.js
+++
@@ -1,7 +0,0 @@
-import {Audio} from 'expo-av';
-import hitSound from '../../../assets/sounds/Gunshot.mp3';
-
-export default playSound = async () => {
-  const {sound} = await Audio.Sound.createAsync(hitSound);
-  await sound.playAsync();
-}
